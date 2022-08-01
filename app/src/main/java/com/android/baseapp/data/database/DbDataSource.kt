2c57e88b34bf64b8cdba8cc1bac086a2bd3f7c5c
IBRAHEM-SALAH <Eng_IbrahemSalah@hotmail.com>
2022-08-01 03:11:28 +0200

    Fixing roomDB import issue.

--- app/src/main/java/com/android/baseapp/data/database/DbDataSource.kt
+++ app/src/main/java/com/android/baseapp/data/database/DbDataSource.kt
@@ -2,9 +2,6 @@ package com.android.baseapp.data.database
 
 import com.android.baseapp.data.model.BaseAppResponse
 
-
 interface DbDataSource {
-
     suspend fun getAllDataInTable(): BaseAppResponse
-
 }
--- app/src/main/java/com/android/baseapp/data/database/DbDataSourceImpl.kt
+++ app/src/main/java/com/android/baseapp/data/database/DbDataSourceImpl.kt
@@ -1,6 +1,5 @@
 package com.android.baseapp.data.database
 
-class DbDataSourceImpl(val appDatabase: AppDatabase) : DbDataSource {
-
+class DbDataSourceImpl(private val appDatabase: AppDatabase) : DbDataSource {
     override suspend fun getAllDataInTable() = appDatabase.baseAppDao().getAllDataInTable()
 }
